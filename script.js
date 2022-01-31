424f25aa918cc5a0e8b9e5a1dc6553ba5f87eb19
Nina <morville5audrey@gmail.com>
2022-01-31 23:41:50 +0100

    add optimisation+ relocation page

--- script.js
+++ script.js
@@ -3,33 +3,52 @@ const MINUTES = 60;
 const HOURS = 60 * MINUTES;
 const DAYS = 24 * HOURS;
 
-const $days = document.getElementById("days");
-const $hours = document.getElementById("hours");
-const $minutes = document.getElementById("minutes");
-const $secondes = document.getElementById("secondes");
+const elements = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    secondes: document.getElementById("secondes")
+}
 
 const countdown = document.querySelector("#countdown");
 const launchDate = Date.parse(countdown.dataset.time) /1000;
 
+let previousDiff = {}
+
 function refreshCountdown(){
     const difference = launchDate - Date.now()/1000;
-
+    if (difference <= 0){
+        document.location = "end-auction.html";
+        return;
+    }
     const diff = {
         days: Math.floor (difference / DAYS),
         hours: Math.floor (difference % DAYS / HOURS),
         minutes: Math.floor (difference % HOURS / MINUTES),
         secondes: Math.floor (difference % MINUTES)
     }
-
-    $days.innerText = diff.days;
-    $hours.innerText = diff.hours;
-    $minutes.innerText = diff.minutes;
-    $secondes.innerText = diff.secondes;
-
     console.log("hello")
+    updateDom(diff);
     window.setTimeout(()=>{
         window.requestAnimationFrame(refreshCountdown)
     }, 1000);
 }
     
+
+/**
+ * Met à jour la structure en fonction d'un interval timeout
+ * @param {{days: number, hours: number, minutes: number, secondes: number}} diff 
+ */
+
+function updateDom(diff){
+    Object.keys(diff).forEach((key)=> {
+        if(previousDiff[key] != diff[key]){
+            elements[key].innerText = diff[key];
+            //console.log(`updating ${key}`);
+        }
+    })  
+previousDiff = diff        
+}
+
+
 refreshCountdown();
