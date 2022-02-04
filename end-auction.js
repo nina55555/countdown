9318e3b00a1697672645c14739d0e245668c119a
Nina <morville5audrey@gmail.com>
2022-02-04 21:58:02 +0100

    fix responsive style

--- end-auction.js
+++ end-auction.js
@@ -3,7 +3,7 @@ function addLove (){
     const lovePic= document.getElementById("love-pic")
     console.log(lovePic)
     const poppin = document.getElementById("love")
-    console.log(poppin)
+    //console.log(poppin)
 
     lovePic.addEventListener('click', () => {
         poppin.innerText = 'much love'
